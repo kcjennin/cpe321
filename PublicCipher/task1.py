9c4fc0f0ac939847b4f25cb0332d9d477e30adad
Zarek Lazowski <96549302+ZarekLazowski@users.noreply.github.com>
2022-05-04 16:49:38 -0700

    Did some thinking for task2

--- PublicCipher/task1.py
+++ PublicCipher/task1.py
@@ -30,8 +30,7 @@ class User:
         decryptor = Cipher(algorithms.AES(self.key), modes.CBC(self.key)).decryptor()
         print((decryptor.update(c) + decryptor.finalize()).decode())
 
-
-if __name__ == '__main__':
+def task1():
     p = int('B10B8F96 A080E01D DE92DE5E AE5D54EC 52C99FBC FB06A3C6 9A6A9DCA 52D23B61 6073E286 75A23D18 9838EF1E 2EE652C0 13ECB4AE A9061123 24975C3C D49B83BF ACCBDD7D 90C4BD70 98488E9C 219A7372 4EFFD6FA E5644738 FAA31A4F F55BCCC0 A151AF5F 0DC8B4BD 45BF37DF 365C1A65 E68CFDA7 6D4DA708 DF1FB2BC 2E4A4371'.replace(" ", ""), 16)
     g = int('A4D1CBD5 C3FD3412 6765A442 EFB99905 F8104DD2 58AC507F D6406CFF 14266D31 266FEA1E 5C41564B 777E690F 5504F213 160217B4 B01B886A 5E91547F 9E2749F4 D7FBD7D3 B9A92EE1 909D0D22 63F80A76 A6A24C08 7A091F53 1DBF0A01 69B6A28A D662A4D1 8E73AFA3 2D779D59 18D08BC8 858F4DCE F97C2A24 855E6EEB 22B3B2E5'.replace(" ", ""), 16)
 
@@ -51,3 +50,43 @@ if __name__ == '__main__':
 
     Bob.receive_text(Alice.send_text('Hi Bob!AAAAAAAAA'))
     Alice.receive_text(Bob.send_text('Hi Alice!AAAAAAA'))
+
+def task2():
+    '''
+    Mallory changes A/B: key becomes SHA-256(0), and you can decrypt both messages.
+    
+    Mallory changes g->1 for Bob: Mallory knows Alice's key, can masquerade as Bob.
+    "" g->p "": 
+    '''
+
+    p = int('B10B8F96 A080E01D DE92DE5E AE5D54EC 52C99FBC FB06A3C6 9A6A9DCA 52D23B61 6073E286 75A23D18 9838EF1E 2EE652C0 13ECB4AE A9061123 24975C3C D49B83BF ACCBDD7D 90C4BD70 98488E9C 219A7372 4EFFD6FA E5644738 FAA31A4F F55BCCC0 A151AF5F 0DC8B4BD 45BF37DF 365C1A65 E68CFDA7 6D4DA708 DF1FB2BC 2E4A4371'.replace(" ", ""), 16)
+    g = int('A4D1CBD5 C3FD3412 6765A442 EFB99905 F8104DD2 58AC507F D6406CFF 14266D31 266FEA1E 5C41564B 777E690F 5504F213 160217B4 B01B886A 5E91547F 9E2749F4 D7FBD7D3 B9A92EE1 909D0D22 63F80A76 A6A24C08 7A091F53 1DBF0A01 69B6A28A D662A4D1 8E73AFA3 2D779D59 18D08BC8 858F4DCE F97C2A24 855E6EEB 22B3B2E5'.replace(" ", ""), 16)
+
+    Alice = User(p, g)
+
+    # Mallory strikes
+    g = 1
+
+    Bob = User(p, g)
+
+    A = Alice.get_C()
+    B = Bob.get_C()
+
+    # machine in middle
+    A = p
+    B = p
+
+    Alice.compute_s(B)
+    Bob.compute_s(A)
+
+    Alice.calc_key()
+    Bob.calc_key()
+
+    assert Alice.key == Bob.key
+
+    Bob.receive_text(Alice.send_text('Hi Bob!AAAAAAAAA'))
+    Alice.receive_text(Bob.send_text('Hi Alice!AAAAAAA'))
+
+
+if __name__ == '__main__':
+    task2()
